7af685f93453e6199e8e2b0f2ffbc990021f9281
nnngrach <nnngrach@gmail.com>
2019-05-15 01:00:28 +0300

    test puppeteer

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
+const puppeteer = require('puppeteer');
 const PORT = process.env.PORT || 5000
 
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -9,20 +11,99 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/times/:x', (req, res) => res.send(showTimes2(req, res)))
-  .get('/pupp', (req, res) => res.send(pupp(req, res)))
+  .get('/getPic', (req, res) => res.send(getPic(req, res)))
+  .get('/getPic2', (req, res) => res.send(getPic2(req, res)))
+  .get('/getPic3', (req, res) => res.send(generateScreenshot()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
+
+const app = express()
+
+  app.get('/api', async (req, res, next) => {
+    let screenshot = await generateScreenshot('https://google.com');
+    let img = Buffer.from(screenshot, 'base64');
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': img.length
+    });
+    res.end(img);
+  });
 
 
-  function showTimes() {
-    return "Show Times";
+app.listen(PORT, () => {
+  console.log('Listening on port ', PORT);
+});
+
+
+
+
+const generateScreenshot = async (url) => {
+  try {
+    //const url = 'https://google.com'
+
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url)
+    await page.setViewport({width: 1920, height: 1080})
+    const image = await page.screenshot({encoding: 'base64'})
+    browser.close()
+    return image
+  } catch (err) {
+    console.error(err.response)
   }
-
-  showTimes2 = (req, res) => {
-    return req.params.x;
-  }
+}
 
 
+function showTimes() {
+  return "Show Times";
+}
 
-  pupp = (req, res) => {
-    return req.params.x;
-  }
+showTimes2 = (req, res) => {
+  return req.params.x;
+}
+
+
+
+
+async function getPic(req, res) {
+  //const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    'args' : [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+    ]
+});
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  //const img = await page.screenshot({path: 'google.png'});
+  const img = await page.screenshot();
+
+
+  var res2 = res
+  await browser.close();
+  res2.statusCode = 200;
+  res2.setHeader('Content-Type', `image/png`);
+  return res2.end(img)
+}
+
+
+async function getPic2(req, res) {
+  //const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    'args' : [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+    ]
+  });
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  //const img = await page.screenshot({path: 'google.png'});
+  const img = await page.screenshot();
+
+  // var res2 = res
+  // await browser.close();
+  // res2.statusCode = 200;
+  // res2.setHeader('Content-Type', `image/png`);
+  //return img
+  return res.end(img)
+}
