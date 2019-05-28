9ce2e40fc6deca76a2dafd213f5969c6a3b34cc5
nnngrach <nnngrach@gmail.com>
2019-05-28 14:55:31 +0300

    optimisation

--- Controller/Modes/OverpassBase.js
+++ Controller/Modes/OverpassBase.js
@@ -6,7 +6,8 @@ async function makeTile( x, y, z ) {
 
     console.log(new Date().getTime() / 1000, ' - start MakeTile func')
 
-  const currentUrl = 'http://overpass-turbo.eu/s/J4Q'
+  //const currentUrl = 'http://overpass-turbo.eu/s/J4Q' // empty request
+  const currentUrl = 'http://overpass-turbo.eu/s/Jph' // for test bbox
   const defaultZoom = 18
 
   const searchFieldSelector = '#search'
@@ -38,31 +39,43 @@ async function makeTile( x, y, z ) {
     console.log(new Date().getTime() / 1000, ' - P goto page')
 
 
+
+
   // Получить координаты краев и центра тайла
   const coordinates = geoTools.getAllCoordinates( x, y, z )
-  const bBox = `(${coordinates.bBox.latMin}, ${coordinates.bBox.lonMin}, ${coordinates.bBox.latMax}, ${coordinates.bBox.lonMax})`;
+  //const bBox = `(${coordinates.bBox.latMin}, ${coordinates.bBox.lonMin}, ${coordinates.bBox.latMax}, ${coordinates.bBox.lonMax})`;
+  const bBox = `[bbox:${coordinates.bBox.latMin}, ${coordinates.bBox.lonMin}, ${coordinates.bBox.latMax}, ${coordinates.bBox.lonMax}];`;
   const centerCoordinates = `${coordinates.center.lat} ${coordinates.center.lon}`
   //console.log(centerCoordinates)
 
     console.log(new Date().getTime() / 1000, ' - P get coordinates')
 
+
+
+
   // Сгенерировать запрос для Оверпасса
+  /*
   //const overpassCode = '(node(50.746,7.154,50.748,7.157););out;>;out skel qt;';
   var overpassCode = '(node;);out;>;out skel qt;'
   overpassCode = overpassCode.replace( 'node', 'node' + bBox )
   overpassCode = overpassCode.replace( 'way', 'way' + bBox )
   overpassCode = overpassCode.replace( 'rel', 'rel' + bBox )
   //console.log(overpassCode)
+  */
+
+
+
+
+
+
 
   console.log(new Date().getTime() / 1000, ' - P replace script')
 
   //Призумиться на центр искомого тайла
-  //await page.focus( searchFieldSelector )
-    console.log(new Date().getTime() / 1000, ' - S focus search field')
-
-
-  //await page.keyboard.type( centerCoordinates )
-
+  console.log(new Date().getTime() / 1000, ' - S focus search field')
+  await page.focus( searchFieldSelector )
+  await page.keyboard.type( centerCoordinates )
+  await page.waitFor( 1000 )
 
 
   // await page.evaluate( () => {
@@ -70,11 +83,20 @@ async function makeTile( x, y, z ) {
   //     example.value = '60.74775258510989 7.156219482421875';
   // });
 
+//await page.focus( searchFieldSelector )
+
+//await page.keyboard.type( centerCoordinates )
+//await page.keyboard.type( '//Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' )
+
+
+
 /*
 await page.evaluate( () => {
     const example = document.getElementById( 'search' );
     example.value = '50.74775258510989 7.156219482421875';
 });
+
+
 
 await page.waitFor( 1000 )
 
@@ -86,7 +108,12 @@ await page.waitFor( 1000 )
 
     console.log(new Date().getTime() / 1000, ' - S typing')
 
-/*
+
+
+
+
+
+
   await page.waitForSelector( searchPopUpMenuSelector , { visible : true } );
     console.log(new Date().getTime() / 1000, ' - S wait for popup menu')
   await page.keyboard.press( 'Enter' )
@@ -116,14 +143,16 @@ await page.waitFor( 1000 )
     }
   }
 
-*/
+
 
 
   // Вставить текст и дождаться, когда IDE распознает синтаксис
 
+  await page.focus( codeEditorSelector )
+  await page.keyboard.type( bBox + ' //' )
 
   // await page.focus( codeEditorSelector )
-    console.log(new Date().getTime() / 1000, ' - E focus on editor')
+  //  console.log(new Date().getTime() / 1000, ' - E focus on editor')
   // await page.keyboard.press( 'End' )
   // await page.keyboard.press( 'Backspace' )
   // await page.keyboard.press( 'Backspace' )
@@ -135,9 +164,9 @@ await page.waitFor( 1000 )
   // await page.keyboard.type( '//Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' )
 
 
-  await page.type(codeEditorSelector,
-    '//Lorem ipsum dolor sit amet',
-  {delay: 0});
+  // await page.type(codeEditorSelector,
+  //   '//Lorem ipsum dolor sit amet',
+  // {delay: 0});
 
 
   // await page.focus( codeEditorSelector )
@@ -154,6 +183,8 @@ await page.waitFor( 1000 )
   //   '//Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   // {delay: 0});
 
+
+
     console.log(new Date().getTime() / 1000, ' - E type Lore ipsum')
   await page.waitFor( 100 )
   //await page.waitForSelector( '#editor > div.CodeMirror.CodeMirror-wrap > div:nth-child(1)' , { visible : true } );
@@ -165,11 +196,15 @@ await page.waitFor( 1000 )
 //await page.evaluate( () => document.getElementById("inputID").value = "")
 //await page.evaluate( () => document.getElementById('#editor > div.CodeMirror.CodeMirror-wrap > div:nth-child(1) > textarea').value = '(node(50.746,7.154,50.748,7.157););out;>;out skel qt;')
 
+/*
 await page.evaluate( () => {
     //const example = document.querySelector( '#editor > div.CodeMirror.CodeMirror-wrap > div:nth-child(1) > textarea' );
     const example = document.querySelector( '#editor > div.CodeMirror.CodeMirror-wrap > div.CodeMirror-scroll.cm-s-default > div > div > div.CodeMirror-lines > div > div:nth-child(5) > pre:nth-child(1) > span' );
     example.value = '(node(50.746,7.154,50.748,7.157););out;>;out skel qt;';
 });
+*/
+
+
 
 
 
@@ -196,8 +231,8 @@ await page.evaluate( () => {
     clip: {x: 489, y: 98, width: 256, height: 256}
   }
 
-  //const screenshot = await page.screenshot(options);
-  const screenshot = await page.screenshot()
+  const screenshot = await page.screenshot(options);
+  //const screenshot = await page.screenshot()
 
     console.log(new Date().getTime() / 1000, ' - I screenshot')
 
