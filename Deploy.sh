1f67f55f93ccfd3bd70f5afbcc82f6c33b2e676b
nnngrach <nnngrach@gmail.com>
2020-05-26 22:01:30 +0300

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -2,7 +2,10 @@
 
 echo "Deploying AnyGIS Mapshoter container to DockerHub"
 
-# ? maybe delete Sharp folder ?
+# To aviod x64 / x86 versions of Sharp image library conflict
+# i just removed it's folder before creating container.
+# This'll be dowlnloaded whith server 'docker pull  &&  docker run' command.
+rm -rf node_modules/sharp
 
 docker build -t nnngrach/mapshoter .
 docker push nnngrach/mapshoter
